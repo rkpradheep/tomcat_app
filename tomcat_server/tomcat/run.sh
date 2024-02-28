9d79d5be8d28c4ad24298b5019a133351247637e
pradheep.rkd <pradheep.rkd@zohocorp.com>
2024-02-28 12:14:42 +0530

    script changes

--- tomcat_server/tomcat/run.sh
+++ tomcat_server/tomcat/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+cd /home/local/ZOHOCORP/pradheep-14225/MyHome/tomcat_build
+
 sed -i 's/8001/8093/' ./bin/setenv.sh >> ./bin/setenv.sh
-sed -i 's/export JAVA_HOME/#export JAVA_HOME/' ./bin/setenv.sh >> ./bin/setenv.sh
+sed -i 's/java-1.17.0-openjdk-amd64/zulu11.64.19-ca-jdk11.0.19-linux_x64/' ./bin/setenv.sh >> ./bin/setenv.sh
 
 
 sed -i 's/port="80"/port="8090"/' ./conf/server.xml >>  ./conf/server.xml
@@ -10,4 +13,4 @@ sed -i 's/port="443"/port="8091"/' ./conf/server.xml >>  ./conf/server.xml
 sed -i 's/tomcat.jks/sas.keystore/' ./conf/server.xml >>  ./conf/server.xml
 sed -i 's/keystorePass="tomcat"/keystorePass="Uwqxh9h11"/' ./conf/server.xml >>  ./conf/server.xml
 
-sh ./bin/catalina.sh jpda run
+sh ./bin/catalina.sh jpda start
