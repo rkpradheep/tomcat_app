79dce5f3685f447b58ebb2b31ffcc32a229fc7ea
pradheep.rkd <pradheep.rkd@zohocorp.com>
2024-05-31 20:37:13 +0530

    job changes

--- tomcat_server/src/com/server/framework/job/MailTask.java
+++ tomcat_server/src/com/server/framework/job/MailTask.java
@@ -1,8 +1,10 @@
 package com.server.framework.job;
 
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONObject;
 
+import com.server.framework.common.Configuration;
 import com.server.framework.common.Util;
 
 public class MailTask
@@ -16,7 +18,11 @@ public class MailTask
 		message += "<br><br><b>Note : This email is sent on behalf of " + jsonObject.getString("from_address") + "</b>";
 		if(jobMeta.isRecurring())
 		{
-			message += "<br><br> Click <a href=\"" + "https://pradheep-14225.csez.zohocorpin.com" + "/scheduler/delete?scheduler_token=" + DigestUtils.sha1Hex(String.valueOf(jobMeta.getId())) + "\">here</a> to unsubscribe";
+			String appUrl = Configuration.getProperty("app.url");
+			if(StringUtils.isNotEmpty(appUrl))
+			{
+				message += "<br><br> Click <a href=\"" + appUrl + "/scheduler/delete?scheduler_token=" + DigestUtils.sha1Hex(String.valueOf(jobMeta.getId())) + "\">here</a> to unsubscribe";
+			}
 		}
 		Util.sendEmail(jsonObject.getString("subject"), jsonObject.getString("to_address"), jsonObject.getString("from_address"), message);
 	}
